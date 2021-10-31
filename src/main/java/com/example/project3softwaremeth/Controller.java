2196b7d033452f956c38844473cf2569aa4a0b77
Manav Patel <manavp4196@gmail.com>
2021-10-31 17:54:02 -0400

    fixed tutionDue for all students

--- src/main/java/com/example/project3softwaremeth/Controller.java
+++ src/main/java/com/example/project3softwaremeth/Controller.java
@@ -336,12 +336,18 @@ public class Controller {
 
     public void calculateTutionDues(ActionEvent actionEvent) {
         if(roster.getSize()==0){
-            messageArea.appendText("roster is empty");
+            messageArea.appendText("roster is empty\n");
         }else{
         for(int i=0;i<roster.getSize();i++){
             if(roster.getStudent(i).getLastPaymentDate()==null && roster.getStudent(i).getTuitionDue()==0 ){
-                roster.getStudent(i).tuitionDue();}
+                roster.getStudent(i).tuitionDue();
+
+                messageArea.appendText("Tuition calculated for all students\n");
+
+            }
+
         }
+
         }
     }
 }
